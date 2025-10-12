9212eb91671320968eeab927f7b504ba3c9476ca
b-x-ubuntu <bart@vmeps.com>
2025-10-12 15:20:15 -0700

    format

--- kinfer_sim/keyboard_listener.py
+++ kinfer_sim/keyboard_listener.py
@@ -1,26 +1,26 @@
+"""Keyboard listener that writes all key presses to queues."""
+
 from queue import Queue
 
 from pynput import keyboard
 
+
 class KeyboardListener:
     """Keyboard listener that writes all key presses to queues."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize keyboard listener with empty queue list and start listening."""
         self.queues: list[Queue] = []
-        
         self.listener = keyboard.Listener(on_press=self._on_press)
         self.listener.daemon = True
         self.listener.start()
 
-    def _on_press(self, key):
-        """ Write all key presses to all queues. """
+    def _on_press(self, key: keyboard.Key) -> None:
+        """Write all key presses to all queues."""
         for queue in self.queues:
             queue.put(str(key).lower())
 
-    def get_queue(self):
-        """ Get a new queue for a listening process """
+    def get_queue(self) -> Queue:
+        """Get a new queue for a listening process."""
         self.queues.append(Queue())
         return self.queues[-1]
-
-
