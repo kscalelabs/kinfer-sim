29b5813f03cf4065554122343cdd95df3f8802b4
b-x-ubuntu <bart@vmeps.com>
2025-10-12 15:17:44 -0700

    dead simple keyboard listener

---
+++ kinfer_sim/keyboard_listener.py
@@ -0,0 +1,29 @@
+import logging
+from queue import Queue
+
+from pynput import keyboard
+
+logger = logging.getLogger(__name__)
+
+class KeyboardListener:
+    """Keyboard listener that writes all key presses to queues."""
+
+    def __init__(self):
+        """Initialize keyboard listener with empty queue list and start listening."""
+        self.queues: list[Queue] = []
+        
+        self.listener = keyboard.Listener(on_press=self._on_press)
+        self.listener.daemon = True
+        self.listener.start()
+
+    def _on_press(self, key):
+        """ Write all key presses to all queues. """
+        for queue in self.queues:
+            queue.put(str(key).lower())
+
+    def get_queue(self):
+        """ Get a new queue for a listening process """
+        self.queues.append(Queue())
+        return self.queues[-1]
+
+
